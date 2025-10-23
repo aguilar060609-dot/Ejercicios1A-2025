c61bcbc5867484d702e9784d568100cd6482f7a1
Luis Aguilar <aguilar060609@gmail.com>
2025-10-23 13:35:25 -0600

    lll

--- Act.js
+++ Act.js
@@ -5,7 +5,6 @@ const prompt = require ("prompt-sync")();
 function Calificacion() {
     let cal = parseInt(prompt("Ingrese la Calificacion: "));
     let respuesta = cal > 6 && cal <8? "Regular" : cal >= 8? "Exelente "
-    console.log(`La calificacion es ${cal} y es ${respuesta}`);
-}
+    console.Log(`La calificacion es${cal} y es ${respuesta}`)
 }
 Calificacion();
